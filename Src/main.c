d2024bbb2637373900f26627377cabcb198f2bb3
Tyrn <orlov@bos-ltd.ru>
2018-03-27 13:46:38 +0300

    Semitrace Off

--- Src/main.c
+++ Src/main.c
@@ -57,7 +57,7 @@ static void MX_GPIO_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
-extern void initialise_monitor_handles(void);
+//extern void initialise_monitor_handles(void);
 
 /* USER CODE END PFP */
 
@@ -95,8 +95,8 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  initialise_monitor_handles();
-  printf("Semihosting initialized!\n"); // In Main
+//  initialise_monitor_handles();
+//  printf("Semihosting initialized!\n"); // In Main
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -109,9 +109,9 @@ int main(void)
 //    fox_pangram();
     cq();
     HAL_Delay(DASH_DURATION * 4);
-    printf("Who are you? Pretty please...\n");
-    char name[50]; scanf("%s", name);
-    printf("Hello, %s!\n", name);
+//    printf("Who are you? Pretty please...\n");
+//    char name[50]; scanf("%s", name);
+//    printf("Hello, %s!\n", name);
   }
   /* USER CODE END 3 */
 
